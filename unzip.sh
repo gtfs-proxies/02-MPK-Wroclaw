98baca68c672dafcdc7c3775861c036fc83e9d22
Artur H. Lange <artur@arc2.dev>
2024-04-06 13:06:05 +0200

    Fix unzipping

--- unzip.sh
+++ unzip.sh
@@ -2,5 +2,5 @@
 
 FILE_LOCATION=/tmp/$FEED_NAME/original
 FEED_FILE=$(ls $FILE_LOCATION | sort -r | head -1)
-
-unzip -u $FILE_LOCATION/$FEED_FILE -d feed/
+echo "GTFS_FILE=$FEED_FILE" >> "$GITHUB_OUTPUT"
+unzip -o $FILE_LOCATION/$FEED_FILE -d feed/
